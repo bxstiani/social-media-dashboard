22fb6505c00e0aef7687787b3ade7dd820506f93
Sebastian <78401571+bxstiani@users.noreply.github.com>
2026-07-22 14:43:34 -0500

    header & main sections styled

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,37 @@
-console.log('hello!');
+const toggle = document.getElementById("theme-toggle");
+const body = document.getElementById("body");
+
+// Apply theme
+function setTheme(theme) {
+  body.setAttribute("class", theme);
+  localStorage.setItem("theme", theme);
+
+  // Keep slider in sync
+  toggle.checked = theme === "dark";
+}
+
+// Determine initial theme
+function initializeTheme() {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme) {
+    setTheme(savedTheme);
+  } else {
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+
+    setTheme(prefersDark ? "dark" : "light");
+  }
+}
+
+initializeTheme();
+
+// Toggle theme
+toggle.addEventListener("change", () => {
+  if (toggle.checked) {
+    setTheme("dark");
+  } else {
+    setTheme("light");
+  }
+});
